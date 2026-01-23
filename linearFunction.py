db884bda578f2e118b1a6abec3a819750e837c56
Aicha Atki <aatki@c4r3p7.1337.ma>
2026-01-24 00:19:39 +0100

    comme meme

--- linearFunction.py
+++ linearFunction.py
@@ -1,61 +1,75 @@
-mileage = []
-realPrice = []
-n = 0
-tetha0 = 0.0
-tetha1 = 0.0
-learningRate = 0.00001
-epochs = 1000
+import json
 
 
 def readData():
-    global mileage, realPrice, n
+    mileage = []
+    realPrice = []
+    
     with open("data.csv", "r") as file:
+        next(file)
         for line in file:
             data = line.strip().split(",")
             mileage.append(int(data[0]))
             realPrice.append(int(data[1]))
-            n += 1
+    return mileage, realPrice, len(mileage)
+
+def normalizeMileage(mileage):
+    max_mileage = max(mileage)
+    normalized = [m / max_mileage for m in mileage]
+    return normalized
 
 
-def estimatePrice(tetha0, tetha1, mileage):
-    return tetha0 + tetha1 * mileage
+def estimatePrice(theta0, theta1, mileage):
+    return theta0 + theta1 * mileage
 
 
-def MSE(n, realPrice, tetha0, tetha1, mileage):
+def MSE(n, realPrice, theta0, theta1, mileage):
     total = 0
     for i in range(n):
-        error = realPrice[i] - estimatePrice(tetha0, tetha1, mileage[i])
+        error = estimatePrice(theta0, theta1, mileage[i]) - realPrice[i]
         total += error ** 2
     return total / n
 
 
-def DerivativeTetha0(n, realPrice, tetha0, tetha1, mileage):
-    total = 0
+
+def DerivativeThetas(n, realPrice, theta0, theta1, mileage):
+    d0, d1 = 0.0, 0.0
     for i in range(n):
-        error = realPrice[i] - estimatePrice(tetha0, tetha1, mileage[i])
-        total += error
-    return (-2 / n) * total
+        error = estimatePrice(theta0, theta1, mileage[i]) - realPrice[i]
+        d0 += error
+        d1 += error * mileage[i]
+    d0 = (-2 / n) * d0
+    d1 = (-2 / n) * d1
+    return d0, d1
 
 
-def DerivativeTetha1(n, realPrice, tetha0, tetha1, mileage):
-    total = 0
-    for i in range(n):
-        error = realPrice[i] - estimatePrice(tetha0, tetha1, mileage[i])
-        total += error * mileage[i]
-    return (-2 / n) * total
+
+def save_thetas(theta0, theta1, max_mileage):
+    data = {
+        "theta0": theta0,
+        "theta1": theta1,
+        "max_mileage": max_mileage
+    }
+    with open("thetas.json", "w") as f:
+        json.dump(data, f)
 
 
 def trainModel():
-    global tetha0, tetha1
-    readData()
+    theta0 = 0.0
+    theta1 = 0.0
+    learningRate = 0.00001
+    epochs = 1000
+    mileage, realPrice, n = readData()
+    normalized_mileage= normalizeMileage(mileage)
 
     for _ in range(epochs):
-        d0 = DerivativeTetha0(n, realPrice, tetha0, tetha1, mileage)
-        d1 = DerivativeTetha1(n, realPrice, tetha0, tetha1, mileage)
+        d0 , d1 = DerivativeThetas(n, realPrice, theta0, theta1, normalized_mileage)
 
-        tetha0 -= learningRate * d0
-        tetha1 -= learningRate * d1
+        theta0 -= learningRate * d0
+        theta1 -= learningRate * d1
+        save_thetas(theta0, theta1, max(mileage))
 
 
-trainModel()
 
+if __name__=="__main__":
+    trainModel()
--- model.py
+++ model.py
@@ -1,20 +1,40 @@
+import json
 
 
-
-
-
-
-
-
-
+def thetas():
+    try: 
+        with open("thetas.json", "r") as f:
+            data = json.load(f)
+    except:
+        print("the thetas file is not valide")
+        exit(-1)
+    return data["theta0"], data["theta1"]
 
 
 
 def main():
-    print("give me the mileage i give u the price")
-    input_value = int(input())
+    theta0 , theta1 = thetas()
+    input_value = None
+    while not input_value:
+            input_value = input("give me the mileage i give u the price\n")
+    try:
+        input_value = float(input_value)
+    except:
+        print("u should give a float as a mileage\n")
+        exit(1)
+
+    if input_value < 0:
+        print("emmm the mileage is negative it make no since don't u think\n")
+        exit(1)
+
+    if input_value >= 409764:
+        print("Your price car is estimated at 0 euros or under.. You should not sell it.")
+        exit(-1)
+
     print("here u are")
-    print(f"{estimatePrice(tetha0, tetha1, input_value):.3f}")
+    normalized_mileage = input_value / 240000
+    estimatePrice = theta0 + theta1 * normalized_mileage
+    print(f"{estimatePrice:.3f}")
 
 
 
